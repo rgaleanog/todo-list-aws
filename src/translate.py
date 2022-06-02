511a87e9c750d9cbb4915a204191aeefe6f3ce86
asrivera80 <104415467+asrivera80@users.noreply.github.com>
2022-06-02 13:08:05 +0200

    Add files via upload

--- src/translate.py
+++ src/translate.py
@@ -6,7 +6,7 @@ import boto3
 
 def translate(event, context):
     # create a response
-    available_language=['af','sq','am','ar','hy','az','bn','bs','bg','ca','zh','zh-TW','hr','cs','da','fa-AF','nl','en','et','fa','tl','fi','fr','fr-CA','ka','de','el','gu','ht','ha','he','hi','hu','is','id','ga','it','ja','kn','kk','ko','lv','lt','mk','ms','ml','mt','mr','mn','no','ps','pl','pt','pt-PT','pa','ro','ru','sr','si','sk','sl','so','es','es-MX','sw','sv','ta','te','th','tr','uk','ur','uz','vi','cy']
+    available_language = ["af","sq","am","ar","hy","az","bn","bs","bg","ca","zh","zh-TW","hr","cs","da","fa-AF","nl","en","et","fa","tl","fi","fr","fr-CA","ka","de","el","gu","ht","ha","he","hi","hu","is","id","ga","it","ja","kn","kk","ko","lv","lt","mk","ms","ml","mt","mr","mn","no","ps","pl","pt","pt-PT","pa","ro","ru","sr","si","sk","sl","so","es"]
     item = todoList.get_item(event['pathParameters']['id'])
     code = event['pathParameters']['lang']
     if item and code in available_language:
