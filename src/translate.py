19f96e8d5d13a5bc3d0dea329b21135b888eea41
asrivera80 <104415467+asrivera80@users.noreply.github.com>
2022-06-03 16:56:39 +0200

    Update translate.py

--- src/translate.py
+++ src/translate.py
@@ -3,11 +3,10 @@ import decimalencoder
 import todoList
 import boto3
 
-
 def translate(event, context):
     item = todoList.get_item(event['pathParameters']['id'])
     code = event['pathParameters']['lang']
-    if item:    
+    if item:
         translate = boto3.client(
             service_name='translate',
             region_name='us-east-1'
