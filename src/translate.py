d54be0e56269f573fcf1436437dd1de71c311676
asrivera80 <104415467+asrivera80@users.noreply.github.com>
2022-06-03 12:48:49 +0200

    Update translate.py

--- src/translate.py
+++ src/translate.py
@@ -3,24 +3,14 @@ import decimalencoder
 import todoList
 import boto3
 
-
 def translate(event, context):
     # create a response
     item = todoList.get_item(event['pathParameters']['id'])
     code = event['pathParameters']['lang']
     if item:
-        translate = boto3.client(
-                service_name='translate',
-                region_name='us-east-1'
-        )
-        result = translate.translate_text(
-            Text=item.text,
-            SourceLanguageCode='auto',
-            TargetLanguageCode=code
-        )
         response = {
             "statusCode": 200,
-            "body": json.dumps(result['TranslatedText'],
+            "body": json.dumps(item + code,
                                cls=decimalencoder.DecimalEncoder)
         }
     else:
