c3a25c3c7fdc2dda42227e00e8ec24fbbc3da09a
asrivera80 <104415467+asrivera80@users.noreply.github.com>
2022-06-03 13:13:21 +0200

    Add files via upload

--- src/translate.py
+++ src/translate.py
@@ -3,19 +3,28 @@ import decimalencoder
 import todoList
 import boto3
 
+
 def translate(event, context):
-    # create a response
     item = todoList.get_item(event['pathParameters']['id'])
     code = event['pathParameters']['lang']
-    if item:
+    if item:    
+        translate = boto3.client(
+            service_name='translate',
+            region_name='us-east-1'
+            )
+        result = translate.translate_text(
+            Text=item['text'],
+            SourceLanguageCode='auto',
+            TargetLanguageCode=code
+        )
         response = {
             "statusCode": 200,
-            "body": json.dumps("OK",
+            "body": json.dumps(result['TranslatedText'],
                                cls=decimalencoder.DecimalEncoder)
         }
-        else:
-            response = {
-                "statusCode": 404,
-                "body": ""
-            }
-            return response
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
