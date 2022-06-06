8bca71bb0ed05e261f1003f64ab45407518994fc
Ubuntu <ubuntu@ip-172-31-82-68.ec2.internal>
2022-06-06 15:06:12 +0000

    funcion traduccion

--- src/translate.py
+++ src/translate.py
@@ -1,20 +1,22 @@
 import todoList
 
-def translate (event, context):
+def translate(event, context):
     item = todoList.get_item(event['pathParameters']['id'])
     if item:
-            result = todoList.translate_item(
-                    item.get('text'),event['pathParameters']['language'])
-            print('TranslatedText: ' + result.get('TranslatedText'))
-            print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
-            print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
-            response = {
-                    "statusCode": 200,
-                    "body": result.get('TranslatedText')
-                    }
-        else:
-            response = {
-                    "statusCode": 404,
-                    "body": ""
-                    }
-            return response
+        result = todoList.translate_item(
+            item.get('text'),
+            event['pathParameters']['language']
+            )
+        print('TranslatedText: ' + result.get('TranslatedText'))
+        print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
+        print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
+        response = {
+            "statusCode": 200,
+            "body": result.get('TranslatedText')
+        }
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
