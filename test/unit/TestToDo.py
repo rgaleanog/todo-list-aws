c3091a819c5ec16ffa3af8f92764ff6e7acdf3e9
javierma84 <javierma84@hotmail.com>
2022-06-13 09:33:26 +0200

    prueba cobertura N

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -87,13 +87,13 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('---------------------')
         print ('Start: test_get_table')
         from src.todoList import get_table
-        self.dynamodb = None
+        #self.dynamodb = None
         #Porqué no llamar directamente pasandole none?
         #table = get_table(self.dynamodb)
         #self.assertIn(table.name,self.table.name)
-        self.assertRaises(Exception, get_table(self.dynamodb))
+        self.assertRaises(Exception, get_table(None))
         #Volver a dejar la tabla como estaba? Mejor hacer un backup?
-        self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        #self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         print ('End: test_get_table')
 
     def test_get_todo(self):
